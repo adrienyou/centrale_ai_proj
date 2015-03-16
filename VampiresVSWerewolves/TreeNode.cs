5e58898307133322779fcba550ab5c4a136ed31f
AYouEcp <adrien.you@student.ecp.fr>
2015-03-16 22:09:38 +0100

    Tree bougé dans main namespace

---
+++ VampiresVSWerewolves/TreeNode.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VampiresVSWerewolves
+{
+    public enum NodeState { Max, Min }
+
+    public class TreeNode<T>
+    {
+        private TreeNode<T> _Parent;
+        private TreeNodeList<T> _Children;
+        private T _Value;
+        private Move _Move;
+        private int _Profondeur;
+        private NodeState _State;
+
+        // Constructors for a TreeNode
+        public TreeNode()
+        {
+            _Parent = null;
+            _Children = new TreeNodeList<T>(this);
+            _Profondeur = 0;
+        }
+
+        public TreeNode(T Value, Move Move, NodeState State)
+        {
+            _Value = Value;
+            _Move = Move;
+            _Parent = null;
+            _Children = new TreeNodeList<T>(this);
+            _Profondeur = 0;
+            _State = State;
+        }
+
+        public TreeNode(T Value, Move Move, TreeNode<T> Parent)
+        {
+            _Value = Value;
+            _Move = Move;
+            _State = Parent.State == NodeState.Max ? NodeState.Min : NodeState.Max;
+            _Parent = Parent;
+            _Children = new TreeNodeList<T>(this);
+            _Profondeur = Parent.Profondeur + 1;
+
+            Parent.Children.Add(this);
+        }
+        
+        // Fields accessors
+        public TreeNode<T> Parent
+        {
+            get { return _Parent; }
+            set
+            {
+                if (value == null)
+                    return;
+                _Parent = value; 
+            }
+        }
+
+        public NodeState State
+        {
+            get { return _State; }
+            set
+            {
+                if (value == null)
+                    return;
+                _State = value;
+            }
+        }
+
+        public Move Move
+        {
+            get { return _Move; }
+            set
+            {
+                if (value == null)
+                    return;
+                _Move = value;
+            }
+        }
+
+        public TreeNodeList<T> Children
+        {
+            get { return _Children; }
+            set
+            {
+                if (value == null)
+                    return;
+
+               _Children = value;
+            }
+        }
+
+        public T Value
+        {
+            get { return _Value; }
+            set
+            {
+                if (value == null && _Value == null)
+                    return;
+
+                if (value != null && _Value != null && value.Equals(_Value))
+                    return;
+
+                _Value = value;
+            }
+        }
+
+        public int Profondeur
+        {
+            get { return _Profondeur; }
+            set
+            {
+                if (value == null)
+                    return;
+
+                _Profondeur = value;
+            }
+        }
+    }
+}
+
---
+++ VampiresVSWerewolves/TreeNodeList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VampiresVSWerewolves
+{
+    public class TreeNodeList<T> : List<TreeNode<T>>
+    {
+        private TreeNode<T> _Parent;
+ 
+        // Constructor
+        public TreeNodeList(TreeNode<T> Parent)
+        {
+            _Parent = Parent;
+        }
+
+        // Add a new TreeNode to the list, set the right parent and return it.
+        public new TreeNode<T> Add(TreeNode<T> Node)
+        {
+            base.Add(Node);
+            Node.Parent = _Parent;
+            Node.State = _Parent.State == NodeState.Max ? NodeState.Min : NodeState.Max;
+            return Node;
+        }
+
+    }
+}
